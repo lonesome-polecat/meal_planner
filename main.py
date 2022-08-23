de94b832a2c113a90ab993cfa17146a9d0cbe777
lonesome-polecat <104938337+lonesome-polecat@users.noreply.github.com>
2022-08-23 11:35:25 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,453 @@
+import openpyxl as xl
+import meal_classes as mc
+import tkinter as tk
+import tkinter.ttk as ttk
+import math
+
+ents = []
+filename = None
+meals_wb = None
+add_more = True
+all_ingredients = []
+store = None
+recipe = None
+recipe_name = None
+meal_days: int = 0
+all_meals = []
+
+class IngWindow(object):
+    def __init__(self):
+        self.fields = mc.get_headers(meals_wb["Ingredients"])
+
+    def main_window(self):
+        self.root = tk.Tk()
+        self.root.title("Ingredient")
+        self.frame = tk.LabelFrame(self.root)
+        self.frame.pack()
+        self.entries = self.makeform()
+        options = mc.get_extra(meals_wb["Ingredients"], "Ingredient")
+        self.entries[0].bind('<KeyRelease>', (lambda event: self.search(self.entries[0], options)))
+        self.entries[0].focus_set()
+        b1 = tk.Button(self.frame, text="Add Ingredient", command=lambda: [self.fetch()])
+        b1.grid(row=5, column=1, padx=10, pady=10)
+        self.root.bind('<Return>', (lambda event: self.fetch()))
+        b2 = tk.Button(self.frame, text="Finish", command=self.root.destroy)
+        b2.grid(row=5, column=0, padx=10, pady=10)
+        self.root.mainloop()
+
+    def makeform(self):
+        entries = []
+        for i, field in enumerate(self.fields):
+            if field == "Category":
+                label = tk.Label(self.frame, text=field)
+                label.grid(row=i, column=0, padx=5, pady=8)
+                options = mc.get_extra(meals_wb["Extra"], "Category")
+                ent = tk.StringVar(value=options[0])
+                drop = ttk.OptionMenu(self.frame, ent, options[0], *options)
+                drop.grid(row=i, column=1, padx=5, pady=8)
+            elif field == "Unit":
+                label = tk.Label(self.frame, text=field)
+                label.grid(row=i, column=0, padx=5, pady=8)
+                options = mc.get_extra(meals_wb["Extra"], "Unit")
+                ent = tk.StringVar(value=options[0])
+                drop = ttk.OptionMenu(self.frame, ent, options[0], *options)
+                drop.grid(row=i, column=1, padx=5, pady=8)
+            elif field == "Store":
+                continue
+            elif field == "Ingredient":
+                label = tk.Label(self.frame, text=field)
+                label.grid(row=i, column=0, padx=5, pady=8)
+                options = mc.get_extra(meals_wb["Ingredients"], "Ingredient")
+                ent = ttk.Combobox(self.frame, values=options)
+                ent.grid(row=i, column=1, padx=5, pady=8)
+            else:
+                label = tk.Label(self.frame, text=field)
+                label.grid(row=i, column=0, padx=5, pady=8)
+                ent = tk.Entry(self.frame)
+                ent.grid(row=i, column=1, padx=5, pady=8)
+            entries.append(ent)
+        return entries
+
+    def fetch(self):
+        self.item_info = []
+        self.headers = []
+        for i, entry in enumerate(self.entries):
+            self.item_info.append(entry.get().lower())
+            self.headers.append(self.fields[i])
+
+        for i, entry in enumerate(self.entries):
+            if type(entry) == tk.Entry:
+                entry.delete(0, "end")
+
+        self.entries[0].focus_set()
+
+        self.item_info.append(store)
+        ing = mc.Ingredient(self.item_info, self.headers)
+        all_ingredients.append(ing)
+
+    def search(self, ent, options):
+        value = ent.get()
+        if value == '':
+            ent['value'] = options
+        else:
+            data = []
+            for item in options:
+                if str(value).lower() in str(item).lower():
+                    data.append(item)
+                    ent['values'] = data
+
+
+class RecipeWindow(object):
+    def __init__(self, name):
+        self.name = name
+        self.fields = mc.get_headers(meals_wb["Recipes"])
+        print(self.fields)
+        self.all_ingredients = []
+        self.isDone = False
+
+    def main_window(self):
+        self.root = tk.Tk()
+        self.root.title("Ingredient")
+        self.frame = tk.LabelFrame(self.root)
+        self.frame.pack()
+        self.entries = self.makeform()
+        options = self.entries[0]['values']
+        self.entries[0].bind('<KeyRelease>', (lambda event: self.search(self.entries[0], options)))
+        self.entries[0].focus_set()
+        b1 = tk.Button(self.frame, text="Add Ingredient", command=lambda: [self.fetch()])
+        b1.grid(row=5, column=1, padx=10, pady=10)
+        self.root.bind("<Return>", (lambda event: self.fetch()))
+        b2 = tk.Button(self.frame, text="Finish", command=lambda: [self.finish(), self.root.destroy()])
+        b2.grid(row=5, column=0, padx=10, pady=10)
+        self.root.mainloop()
+
+    def makeform(self):
+        # Make 'Ingredient' field a combobox with values from 'ingredients' sheet
+        entries = []
+        for i, field in enumerate(self.fields):
+            if field == "Unit":
+                label = tk.Label(self.frame, text=field)
+                label.grid(row=i, column=0, padx=5, pady=8)
+                options = mc.get_extra(meals_wb["Extra"], "Cooking Unit")
+                ent = tk.StringVar(value=options[0])
+                drop = ttk.OptionMenu(self.frame, ent, options[0], *options)
+                drop.grid(row=i, column=1, padx=5, pady=8)
+            elif field == "Recipe":
+                continue
+            elif field == "Ingredient":
+                label = tk.Label(self.frame, text=field)
+                label.grid(row=i, column=0, padx=5, pady=8)
+                options = mc.get_extra(meals_wb["Ingredients"], "Ingredient")
+                ent = ttk.Combobox(self.frame, values=options)
+                ent.grid(row=i, column=1, padx=5, pady=8)
+            else:
+                label = tk.Label(self.frame, text=field)
+                label.grid(row=i, column=0, padx=5, pady=8)
+                ent = tk.Entry(self.frame)
+                ent.grid(row=i, column=1, padx=5, pady=8)
+            entries.append(ent)
+        return entries
+
+    def fetch(self):
+        self.item_info = []
+        self.headers = []
+        for i, entry in enumerate(self.entries):
+            self.item_info.append(entry.get().lower())
+            self.headers.append(self.fields[i+1])
+
+        for i, entry in enumerate(self.entries):
+            if type(entry) == tk.Entry or type(entry) == ttk.Combobox:
+                entry.delete(0, "end")
+
+        self.entries[0].focus_set()
+
+        ing = mc.Ingredient(self.item_info, self.headers)
+        print(f"{ing.name[0]} {str(ing.amount[0])} {ing.unit[0]}")
+        self.all_ingredients.append(ing)
+
+    def search(self, ent, options):
+        value = ent.get()
+        if value == '':
+            ent['value'] = options
+        else:
+            data = []
+            for item in options:
+                if str(value).lower() in str(item).lower():
+                    data.append(item)
+                    ent['values'] = data
+
+    def finish(self):
+        self.isDone = True
+
+
+class MealPlanWindow(object):
+    def __init__(self):
+        self.fields = mc.get_headers(meals_wb["Schedule"])
+        self.all_recipes = []
+        self.all_ingredients = []
+
+
+    def main_window(self):
+        self.root = tk.Tk()
+        self.root.title("Schedule")
+        self.frame = tk.LabelFrame(self.root)
+        self.frame.pack()
+        self.entries = self.makeform()
+        self.root.bind("<Return>", (lambda event: [self.fetch(), self.root.destroy()]))
+        b2 = tk.Button(self.frame, text="Finish", command=lambda: [self.fetch(), self.root.destroy()])
+        b2.grid(column=1, padx=10, pady=10)
+        self.root.mainloop()
+
+
+    def makeform(self):
+        entries = []
+        for i in range(int(meal_days)):
+            label = tk.Label(self.frame, text=f"Meal {i+1}")
+            label.grid(row=i, column=0, padx=5, pady=8)
+            options = mc.get_recipes(meals_wb["Recipes"])
+            ent = ttk.Combobox(self.frame, values=options)
+            ent.grid(row=i, column=1, padx=5, pady=8)
+            ent.bind('<KeyRelease>', (lambda event: self.search(ent, options)))
+            entries.append(ent)
+        return entries
+
+# Meal Plan Window
+    def fetch(self):
+        for entry in self.entries:
+            self.all_recipes.append(entry.get())
+
+        global all_meals
+        all_meals = self.all_recipes
+        ws1 = meals_wb["Recipes"]
+        headers = mc.get_headers(ws1)
+        for i in range(1, ws1.max_row+1):
+            cell = ws1.cell(row=i+1, column=1).value
+            print(cell)
+            if cell is not None:
+                if cell in self.all_recipes:
+                    j = i
+                    while ws1.cell(row=j+1, column=2).value != None:
+                        print("I got it!!!")
+                        row = j+1
+                        name = ws1.cell(row=row, column=headers.index("Ingredient")+1).value
+                        amount = ws1.cell(row=row, column=headers.index("Amt")+1).value
+                        unit = ws1.cell(row=row, column=headers.index("Unit")+1).value
+                        item_info = [name, amount, unit]
+                        my_ing = mc.Ingredient(item_info, headers[1:])
+                        print("Printing name of ing")
+                        print(my_ing.name)
+                        self.all_ingredients.append(my_ing)
+                        print(len(self.all_ingredients))
+                        j += 1
+
+
+    def search(self, ent, options):
+        value = ent.get()
+        if value == '':
+            ent['value'] = options
+        else:
+            data = []
+            for item in options:
+                if str(value).lower() in str(item).lower():
+                    data.append(item)
+                    ent['values'] = data
+
+
+def fetch(ent, index=0):
+    global store
+    global recipe_name
+    global meal_days
+    if index == 1:
+        store = ent.get()
+    elif index == 2:
+        recipe_name = ent.get()
+    elif index == 3:
+        meal_days = ent.get()
+
+
+def store_prompt():
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack(side=tk.TOP, padx=5, pady=5)
+    label = tk.Label(frame, text="Enter the name of the store you shop at")
+    label.pack(side=tk.TOP, padx=10, pady=10)
+    options = mc.get_extra(meals_wb["Extra"], "Store")
+    ent = ttk.Combobox(frame, values=options)
+    ent.pack(side=tk.TOP, padx=10, pady=10)
+    ent.focus_force()
+    b1 = tk.Button(frame, text="Continue", command=lambda: [fetch(ent, 1), root.destroy(), add_ingredients()])
+    b1.pack(side=tk.BOTTOM, padx=10, pady=10)
+    root.bind('<Return>', (lambda event: [fetch(ent, 1), root.destroy(), add_ingredients()]))
+# In class windows have focus_set()
+
+def recipe_name_prompt():
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack(side=tk.TOP, padx=5, pady=5)
+    label = tk.Label(frame, text="Enter the name of the recipe")
+    label.pack(side=tk.TOP, padx=10, pady=10)
+    ent = tk.Entry(frame)
+    ent.pack(side=tk.TOP, padx=10, pady=10)
+    ent.focus_force()
+    b1 = tk.Button(frame, text="Continue", command=lambda: [fetch(ent, 2), root.destroy(), add_recipe()])
+    b1.pack(side=tk.BOTTOM, padx=10, pady=10)
+    root.bind('<Return>', (lambda event: [fetch(ent, 2), root.destroy(), add_recipe()]))
+
+
+def meal_plan_prompt():
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack(side=tk.TOP, padx=5, pady=5)
+    label = tk.Label(frame, text="How many days are you planning for?")
+    label.pack(side=tk.TOP, padx=10, pady=10)
+    ent = tk.Entry(frame)
+    ent.pack(side=tk.TOP, padx=10, pady=10)
+    ent.focus_force()
+    b1 = tk.Button(frame, text="Continue", command=lambda: [fetch(ent, 3), root.destroy(), meal_plan()])
+    b1.pack(side=tk.BOTTOM, padx=10, pady=10)
+    root.bind('<Return>', (lambda event: [fetch(ent, 3), root.destroy(), meal_plan()]))
+
+
+def meal_plan():
+    meals = MealPlanWindow()
+    meals.main_window()
+    upload_meals(meals)
+
+
+def add_recipe():
+    global recipe
+    recipe = RecipeWindow(recipe_name)
+    recipe.main_window()
+    if recipe.isDone:
+        upload_recipe()
+        print("Uploaded recipe")
+
+
+def add_ingredients():
+    ing_window = IngWindow()
+    ing_window.main_window()
+    upload_ingredients()
+
+
+def upload_ingredients():
+    ing_ws = meals_wb["Ingredients"]
+    headers = mc.get_headers(ing_ws)
+    preloaded_ings = mc.get_extra(ing_ws, "Ingredient")
+    for i, ingredient in enumerate(all_ingredients):
+        row = ing_ws.max_row
+        if ingredient.name[0] in preloaded_ings:
+            row = preloaded_ings.index(ingredient.name[0])+1
+        for j, header in enumerate(headers):
+            ing_ws.cell(row=row+1, column=j+1).value = ingredient.item_info[j]
+
+    meals_wb.save(filename)
+
+
+def upload_recipe():
+    global recipe
+    ws1 = meals_wb["Recipes"]
+    ws2 = meals_wb["Ingredients"]
+    ingredients = mc.get_extra(meals_wb["Ingredients"], "Ingredient")
+    headers = mc.get_headers(ws1)
+    row = ws1.max_row + 1
+    ws2_row = ws2.max_row + 1
+    if row > 3:
+        row += 1
+    ws1.cell(row=row, column=headers.index("Recipe")+1).value = recipe.name
+    for ing in recipe.all_ingredients:
+        ws1.cell(row=row, column=headers.index(ing.name[1])+1).value = ing.name[0]
+        ws1.cell(row=row, column=headers.index(ing.amount[1])+1).value = ing.amount[0]
+        ws1.cell(row=row, column=headers.index(ing.unit[1])+1).value = ing.unit[0]
+        if ing.name[0].lower() not in ingredients:
+            ws2.cell(row=ws2_row, column=1).value = ing.name[0]
+            ws2.cell(row=ws2_row, column=2).value = "other"
+            ws2.cell(row=ws2_row, column=3).value = 0
+            ws2.cell(row=ws2_row, column=4).value = "none"
+            ws2.cell(row=ws2_row, column=5).value = 1.00
+            ws2_row += 1
+        row += 1
+
+    meals_wb.save(filename)
+
+
+def upload_meals(meals):
+    print("Uploading...")
+    rec_ings = meals.all_ingredients
+    print(len(rec_ings))
+    store_ings = mc.get_ingredients(meals_wb['Ingredients'])
+    ws1 = meals_wb['ShoppingList']
+    headers = mc.get_headers(ws1)
+    prices = []
+    column = ""
+    row = ws1.max_row + 1
+    for i, rec_ing in enumerate(rec_ings):
+        isFound = False
+        for j, store_ing in enumerate(store_ings):
+            store_ing.category[0] = store_ing.category[0].capitalize()
+            if store_ing.name[0].lower() == rec_ing.name[0].lower():
+                print("I found the ingredient!")
+                #print(store_ing.category[0])
+                amount, price, unit = mc.conversion(rec_ing, store_ing, meals_wb['Weights'])
+                if store_ing.category[0] == "Grains" or store_ing.category[0] == "Can" or store_ing.category[0] == "Bottle":
+                    store_ing.category[0] = "Grains/Cans/Bottles"
+                column = headers.index(store_ing.category[0])+1
+                for k in range(1, row+1):
+                    print(f"k = {k}, column = {column}, row = {row}")
+                    if ws1.cell(row=k, column=column).value is None:
+                        temp_row = k
+                        break
+                ws1.cell(row=temp_row, column=column).value = store_ing.name[0]
+                ws1.cell(row=temp_row, column=column+1).value = f"{str(amount)} {unit}"
+                prices.append(price)
+                isFound = True
+        if not isFound:
+            print("I did not find the ingredient")
+            column = headers.index(rec_ing.category[0])+1
+            for k in range(1, row+1):
+                if ws1.cell(row=k, column=column).value is None:
+                    temp_row = k
+                    break
+            ws1.cell(row=temp_row, column=column).value = rec_ing.name[0]
+            ws1.cell(row=temp_row, column=column+1).value = f"{str(rec_ing.amount[0])} {rec_ing.unit[0]}"
+        row += 1
+
+    ws2 = meals_wb['Schedule']
+    new_headers = mc.get_headers(ws2)
+    for i, meal in enumerate(all_meals):
+        ws2.cell(row=i+2, column=new_headers.index("Meal")+1).value = meal
+
+    total_price = 0
+    for i in range(len(prices)):
+        #print(prices[i])
+        prices[i] = float(prices[i])
+        total_price += prices[i]
+    ws2.cell(row=2, column=new_headers.index("Total")+1).value = total_price
+
+    meals_wb.save(filename)
+
+
+
+
+def start_window():
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack(side=tk.TOP, padx=5, pady=5)
+    label = tk.Label(frame, text="Choose an option")
+    label.pack(side=tk.TOP, padx=10, pady=10)
+    b1 = tk.Button(frame, text="Meal Plan", command=lambda: [root.destroy(), meal_plan_prompt()])
+    b2 = tk.Button(frame, text="Add Recipe", command=lambda: [root.destroy(), recipe_name_prompt()])
+    b3 = tk.Button(frame, text="Add Ingredients", command=lambda: [root.destroy(), store_prompt()])
+    b3.pack(side=tk.BOTTOM, padx=5, pady=5)
+    b2.pack(side=tk.BOTTOM, padx=5, pady=5)
+    b1.pack(side=tk.BOTTOM, padx=5, pady=5)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    filename = r"C:\Users\helam\Box\Bennions\meal_plan.xlsx"
+    try:
+        meals_wb = xl.load_workbook(filename)
+    except:
+        print("Could not find file '%s'" % filename)
+
+    start_window()
---
+++ meal_classes.py
@@ -0,0 +1,125 @@
+import openpyxl as xl
+import math
+
+class Ingredient(object):
+    def __init__(self, item_columns, headers, row=0):
+        self.row = row + 1
+        self.item_info = item_columns
+        self.headers = headers
+        self.category = ["Other", ""]
+        for i, header in enumerate(headers):
+            if header == "Ingredient":
+                self.name = [item_columns[i], header]
+            if header == "Category":
+                self.category = [item_columns[i], header]
+            if header == "Amt":
+                self.amount = [item_columns[i], header]
+            if header == "Unit" or header == "Cooking Unit":
+                self.unit = [item_columns[i], header]
+            if header == "Price":
+                self.price = [item_columns[i], header]
+            if header == "Store":
+                self.store = [item_columns[i], header]
+
+
+
+class Recipe(object):
+    def __init__(self, name, ingredients):
+        self.row = 0
+        self.name = name
+        self.ingredients = ingredients
+
+
+def get_headers(worksheet):
+    column = worksheet.max_column
+    headers = []
+    for i in range(column):
+        header = worksheet.cell(row=1, column=i+1).value
+        headers.append(header)
+
+    return headers
+
+
+def get_ingredients(worksheet):
+
+    row = worksheet.max_row
+    column = worksheet.max_column
+    headers = get_headers(worksheet)
+    ingredients = []
+
+    for i in range(row):
+        ing_columns = []
+        for j in range(column):
+            ing_columns.append(worksheet.cell(row=i+1, column=j+1).value)
+
+        ingredient = Ingredient(ing_columns, headers, i)
+        ingredients.append(ingredient)
+
+    return ingredients
+
+
+def get_recipes(worksheet):
+    recipes = []
+    row = worksheet.max_row
+    for i in range(1, row+1):
+        if worksheet.cell(row=i+1, column=1).value is not None:
+            recipes.append(worksheet.cell(row=i+1, column=1).value)
+
+    return recipes
+
+def get_extra(worksheet, header):
+    column = worksheet.max_column
+    col = 0
+    extra_info = []
+    for i in range(column):
+        if header == worksheet.cell(row=1, column=i+1).value:
+            col = i+1
+            break
+    if col != 0:
+        for i in range(worksheet.max_row-1):
+            value = worksheet.cell(row=i+2, column=col).value
+            if value is None:
+                continue
+            extra_info.append(value)
+    else:
+        print("Error: Could not find column header '%s'" % header)
+        return False
+    return extra_info
+
+def conversion(rec_ing, store_ing, worksheet=None):
+    # from recipe_ingredients to store_ingredients
+    print("Store ingredient price is " + str(store_ing.price[0]))
+    print("Recipe amount is " + str(rec_ing.amount[0]))
+    amount = float(rec_ing.amount[0])
+    store_ing.price[0] = float(store_ing.price[0])
+    if rec_ing.unit[0] == "can" and store_ing.unit[0] == "can":
+        return amount, amount * store_ing.price[0], store_ing.unit[0]
+    elif rec_ing.unit[0] == "whole" and store_ing.unit[0] == "whole":
+        return amount, amount * store_ing.price[0], store_ing.unit[0]
+    elif rec_ing.unit[0] == "cup" and store_ing.unit[0] == "can":
+        amount = math.ceil(rec_ing.amount[0] * 237/400)
+        return amount, amount * store_ing.price[0], store_ing.unit[0]
+    elif rec_ing.unit[0] == "whole" and store_ing.unit[0] == "lbs":
+        amount, price, unit = convert_to_lbs(worksheet, rec_ing, store_ing.price[0])
+        return amount, price, unit
+    elif rec_ing.unit[0] == "lbs" and store_ing.unit[0] == "lbs":
+        return amount, amount * store_ing.price[0], store_ing.unit[0]
+    else:
+        return rec_ing.amount[0], store_ing.price[0], rec_ing.unit[0]
+
+# 237 mL/cup
+# 15 mL/tbsp
+# 5 mL/tsp
+# 3750 mL/gal
+
+def convert_to_lbs(worksheet, rec_ing, price):
+    ws1 = worksheet
+    for i in range(1, ws1.max_row+1):
+        if rec_ing.name[0] == ws1.cell(row=i, column=1).value:
+            row = i
+            break
+    amount = rec_ing.amount * ws1.cell(row=row, column=3).value
+    price = amount * price
+    return amount, "lbs", price
+
+
